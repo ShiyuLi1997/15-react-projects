fd0a8de1c91359aa2bdcdffce60c0c2a7762a319
Shiyu Li <lishiyu6@msu.edu>
2022-10-18 13:12:59 -0400

    05 navbar

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,55 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState, useEffect } from "react";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+	console.log("items: ", items);
+	let [Data, setData] = useState(items);
+	let [arr, setArr] = useState([]);
+	let [cate, setCate] = useState([]);
+	// with set
+	useEffect(() => {
+		const categorySet = new Set(
+			Data.map((e) => {
+				return e.category;
+			})
+		);
+		setCate(Array.from(categorySet));
+	}, []);
+	// with array
+	useEffect(() => {
+		const arr = [];
+		Data.map((e) => {
+			if (!arr.includes(e.category)) arr.push(e.category);
+		});
+		console.log(arr);
+		setArr(arr);
+	}, []);
+	return (
+		<div
+			className="navbar"
+			style={{
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "center",
+				gap: "20px",
+			}}
+		>
+			{cate.map((e) => {
+				return (
+					<p className="cate" key={e}>
+						{e}
+					</p>
+				);
+			})}
+			{arr.map((e) => {
+				return (
+					<p className="cate" key={e}>
+						{e}
+					</p>
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
